63997cbda42377d49680d4fafbd0ccc26e9d5c41
Andre Vallestero <andrevallestero@gmail.com>
2024-06-17 00:17:03 -0400

    Disable input based player movement

--- main.gd
+++ main.gd
@@ -21,14 +21,7 @@ func _ready():
 	spawn_fruit()
 	
 func _input(event):
-	if event is InputEventMouseMotion:
-		var plane_pos = DROP_PLANE.intersects_ray(
-			camera.project_ray_origin(event.position),
-			camera.project_ray_normal(event.position)
-		)
-		if plane_pos:
-			player.position = player_clamp(current_radius, plane_pos)
-	elif event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		current_fruit.reparent(self)
 		current_fruit.freeze = false
 		current_fruit.find_child("CollisionShape3D").set_deferred("disabled", false)
@@ -36,7 +29,15 @@ func _input(event):
 
 # Called every frame
 func _process(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	var plane_pos = DROP_PLANE.intersects_ray(
+		camera.project_ray_origin(mouse_pos),
+		camera.project_ray_normal(mouse_pos)
+	)
+	if plane_pos:
+		player.position = player_clamp(current_radius, plane_pos)
 	marker.position.y = -10 * marker_shape_cast.get_closest_collision_safe_fraction() - current_radius
+	
 	var input_dir = Input.get_axis("ui_left", "ui_right")
 	if input_dir:
 		orbit_pos = fmod(orbit_pos + input_dir * SPEED * delta, TAU)
