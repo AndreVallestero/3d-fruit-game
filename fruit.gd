0b6c14a565bd00c2820dba4262b941f9dad56d3c
Andre Vallestero <andrevallestero@gmail.com>
2024-06-17 12:05:26 -0400

    Add combining logic

--- fruit.gd
+++ fruit.gd
@@ -4,14 +4,26 @@ const SQRT2 = sqrt(2)
 const tier_mesh = [
 	preload("res://meshes/mesh_godot.tres"),
 	preload("res://meshes/mesh_blender.tres"),
-	preload("res://meshes/mesh_tux.tres")
+	preload("res://meshes/mesh_tux.tres"),
+	preload("res://meshes/mesh_krita.tres"),
+	preload("res://meshes/mesh_firefox.tres")
 ]
 
 var tier
+var radius
 	
-func set_tier(tier: int):
-	self.tier = tier
+func set_tier(init_tier: int):
+	self.tier = init_tier
+	self.radius = SQRT2 ** init_tier / 2
 
 func _ready():
 	$MeshInstance3D.set_mesh(tier_mesh[tier])
-	$CollisionShape3D.shape.radius = SQRT2 ** tier / 2
+	$CollisionShape3D.shape.radius = self.radius
+
+func update_tier(new_tier: int):
+	set_tier(new_tier)
+	_ready()
+	
+func _on_body_entered(body):
+	if body is RigidBody3D:
+		get_parent().collision_detected(self, body as RigidBody3D)
--- main.gd
+++ main.gd
@@ -1,6 +1,5 @@
 extends Node3D
 
-const TAU = PI * 2
 const SPEED = TAU / 3
 const DROP_PLANE = Plane(0, 1, 0, 5)
 
@@ -35,8 +34,8 @@ func _process(delta):
 		camera.project_ray_normal(mouse_pos)
 	)
 	if plane_pos:
-		player.position = player_clamp(current_radius, plane_pos)
-	marker.position.y = -10 * marker_shape_cast.get_closest_collision_safe_fraction() - current_radius
+		player.position = player_clamp(plane_pos)
+	marker.position.y = -10 * marker_shape_cast.get_closest_collision_safe_fraction() - current_fruit.radius
 	
 	var input_dir = Input.get_axis("ui_left", "ui_right")
 	if input_dir:
@@ -45,16 +44,28 @@ func _process(delta):
 		camera.position.z = cos(orbit_pos) * 10
 		camera.rotation.y = orbit_pos
 
-func player_clamp(fruit_radius, pos):
+func player_clamp(pos):
 	return Vector3(
-		clamp(pos.x, -5 + fruit_radius, 5 - fruit_radius),
+		clamp(pos.x, -5 + current_fruit.radius, 5 - current_fruit.radius),
 		5,
-		clamp(pos.z, -5 + fruit_radius, 5 - fruit_radius)
+		clamp(pos.z, -5 + current_fruit.radius, 5 - current_fruit.radius)
 	)
 	
 func spawn_fruit():
 	current_fruit = fruit_scene.instantiate()
 	current_fruit.set_tier(randi_range(0, 2))
-	current_radius = current_fruit.find_child("CollisionShape3D").shape.radius
+	current_fruit.rotation = Vector3(randf() * TAU, randf() * TAU, randf() * TAU)
 	player.add_child(current_fruit)
+	marker.mesh.outer_radius = current_fruit.radius
+	marker.mesh.inner_radius = current_fruit.radius / 2 -0.01
+	
+func collision_detected(fruit1: RigidBody3D, fruit2: RigidBody3D):
+	if fruit1.tier == fruit2.tier:
+		var midpoint = fruit1.position + 0.5 * (fruit2.position - fruit1.position)
+		remove_child(fruit2)
+		fruit2.queue_free()
+		fruit1.position = midpoint
+		fruit1.update_tier(fruit1.tier + 1)
+		fruit1.angular_velocity = Vector3.ZERO
+		fruit1.linear_velocity = Vector3.ZERO
 	
