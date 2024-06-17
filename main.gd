b6e69ad7af43a43d16bc99e95ce2a0643b278d6e
Andre Vallestero <andrevallestero@gmail.com>
2024-06-16 23:58:24 -0400

    Optimize frame loop

--- main.gd
+++ main.gd
@@ -21,29 +21,29 @@ func _ready():
 	spawn_fruit()
 	
 func _input(event):
-	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
-		current_fruit.reparent(self)
-		current_fruit.freeze = false
-		current_fruit.find_child("CollisionShape3D").set_deferred("disabled", false)
-		spawn_fruit()
+	if event.position != null:
+		var plane_pos = DROP_PLANE.intersects_ray(
+			camera.project_ray_origin(event.position),
+			camera.project_ray_normal(event.position)
+		)
+		if plane_pos:
+			player.position = player_clamp(current_radius, plane_pos)
+	if event is InputEventMouseButton:
+		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+			current_fruit.reparent(self)
+			current_fruit.freeze = false
+			current_fruit.find_child("CollisionShape3D").set_deferred("disabled", false)
+			spawn_fruit()
 
 # Called every frame
 func _process(delta):
+	marker.position.y = -10 * marker_shape_cast.get_closest_collision_unsafe_fraction() - current_radius
 	var input_dir = Input.get_axis("ui_left", "ui_right")
-	orbit_pos = fmod(orbit_pos + input_dir * SPEED * delta, TAU)
-	camera.position.x = sin(orbit_pos) * 10
-	camera.position.z = cos(orbit_pos) * 10
-	camera.rotation.y = orbit_pos
-	
-	var mouse_position = camera.get_viewport().get_mouse_position()
-	var plane_pos = DROP_PLANE.intersects_ray(
-		camera.project_ray_origin(mouse_position),
-		camera.project_ray_normal(mouse_position)
-	)
-	if plane_pos:
-		player.position = player_clamp(current_radius, plane_pos)
-		marker.position.y = -10 * marker_shape_cast.get_closest_collision_unsafe_fraction() - current_radius
-	
+	if input_dir:
+		orbit_pos = fmod(orbit_pos + input_dir * SPEED * delta, TAU)
+		camera.position.x = sin(orbit_pos) * 10
+		camera.position.z = cos(orbit_pos) * 10
+		camera.rotation.y = orbit_pos
 
 func player_clamp(fruit_radius, pos):
 	return Vector3(
